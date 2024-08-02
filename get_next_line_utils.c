dfb0815fcf0df3400a543d678495391b7a4bee7e
Oleksii Reshetnyk <oleksiireshetnyk@gmail.com>
2024-08-02 10:49:55 +0200

    paco correct/no leaks

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: olreshet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 17:47:13 by olreshet          #+#    #+#             */
-/*   Updated: 2024/08/02 05:41:33 by olreshet         ###   ########.fr       */
+/*   Updated: 2024/08/02 06:54:58 by yourLogin        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*str;
 
-	if (!s1 && !s2)
-		return (NULL);
 	if (!s1)
 		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
 	i = 0;
-	j = 0;
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (0);
@@ -62,9 +57,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		str[i] = s1[i];
 		i++;
 	}
+	j = 0;
 	while (s2[j] != '\0')
 		str[i++] = s2[j++];
 	str[i] = '\0';
+	free((void *)s1);
 	return (str);
 }
 
